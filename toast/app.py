6a4fa5602f85e40e9eda6c1504e77c20e17682c9
Dan Riti <dmriti@gmail.com>
2013-03-20 00:29:40 -0400

    Add json endpoint example.

--- toast/app.py
+++ toast/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/json/example")
+def get_json():
+    return jsonify(name='toast',
+                   count=1)
+
 if __name__ == "__main__":
+    # Development only! Reloads server on file change.
+    app.debug = True
     app.run()
