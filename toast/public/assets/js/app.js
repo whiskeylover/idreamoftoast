41be8ea3f1dff6c5f7df1fe51c89205786987592
Dan Riti <dmriti@gmail.com>
2013-05-18 12:51:07 -0400

    Remove custom template tags in favor of using Underscore defaults. #17

--- toast/public/assets/js/app.js
+++ toast/public/assets/js/app.js
@@ -1,12 +1,5 @@
 $(function() {
 
-    // Magic underscore settings to allow underscore templates to play
-    // nicely with Rails ERB templates!
-    _.templateSettings = {
-        interpolate: /\{\{\=(.+?)\}\}/g,
-        evaluate: /\{\{(.+?)\}\}/g
-    };
-
 ////////
 // Views
 ////////
